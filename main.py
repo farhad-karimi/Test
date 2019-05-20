a40887b4a4df5b0afb5e6d858ec3c98b95c41d83
Farhad <farhadk@gmail.com>
2019-05-20 09:42:08 -0700

    Test5

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-print ('Hello World!')
-print ('This is the master branch!')
-
-
-print ('This is branch1 ammendment!')
-
-import numpy as np
-from scipy import linalg as la
-
-A = np.eye(3)
-A[0,2] = 3
-A[2,0] = 3
-print (A)
-
-e, v = la.eig(A)
-print(e)
+print ('Hello World! This is the master branch!')
