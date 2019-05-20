b27c979d9132b20dea771b4d70e58254de154282
Farhad <farhadk@gmail.com>
2019-05-20 10:53:46 -0700

    Add eig calculation!

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import numpy as np
 from scipy import linalg as la
 
 print ('Hello World! This is the master branch!')
+
+print ('Here goes the TestBranch!')
+A = np.eye(3)
+A [0,2] = 3
+A [2,0] = 3
+print (A)
+
+e , v = la.eig(A)
+print(e)
+print('Success!')
